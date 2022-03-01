0433803b11fcd1289a5d0c59058f8de3ac949689
chrilona <christabelalona@gmail.com>
2022-03-01 22:28:06 +0300

    1st MARCH

---
+++ src/MAINN.kt
@@ -0,0 +1,30 @@
+fun main(){
+    var a = "akirachix"
+    println(a[0].toString() + a[2] + a[3])
+    len()
+    structure("lona",54)
+    confirm()
+
+}
+fun structure(n:String,age:Int){
+    var text= "Hi, my name is $n and iam $age years old"
+    println(text)
+
+}
+fun len(){
+    var b = "Terrific Tuesday"
+    var c = b.length
+    println(c)
+
+}
+fun confirm(){
+    var j = "LONA"
+    if (j=="LONA"){
+        println("That is my name!")
+    }
+    else{
+        println("I  DO NOT KNOW WHO THAT IS!")
+    }
+}
+
+
